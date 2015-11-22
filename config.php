0220dbecd824c1ae142c06812381dd4d43ac62a7
Jorge Leonel <jleonel@gmail.com>
2015-11-22 02:40:19 -0200

    primeiro commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+	session_start();
+	$accountsid = '';	//	 TWILIO ACCOUNT SID
+	$authtoken = '';	//	 TWILIO AUTH TOKEN
+	$fromNumber = '';	//	Numero de telefone de origem
+
+
+	$dbhost = '';	//	 DB HOST
+	$dbname = '';	//	 DB NAME
+	$dbuser = '';	//	 DB USER
+	$dbpass = '';	//	 DB PASS
+?>
